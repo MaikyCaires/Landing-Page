dd02f4f4c4ce1dcd6faf14879a6a283e35e59b3f
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-08-02 18:26:58 -0300

    Inicio do projeto

---
+++ javascript/module/menuMobile.js
@@ -0,0 +1,28 @@
+export default function menuMobile () {
+    
+}
+
+const btn = document.querySelector('[data-mobile="button"]')
+const menu = document.querySelector('[data-links="menu"]')
+const container = document.querySelector('body')
+
+function mostrar(){
+    menu.classList.toggle("mostrarMenu")
+    if(menu.classList.contains("mostrarMenu")){
+        container.addEventListener("click", fecharMenu)
+    }else{
+        container.removeEventListener("click", fecharMenu)
+    }
+}
+
+function fecharMenu(event){
+    if(event.target != btn){
+        menu.classList.remove("mostrarMenu")
+    }
+}
+
+
+btn.addEventListener("click", mostrar)
+
+
+console.log(btn)
