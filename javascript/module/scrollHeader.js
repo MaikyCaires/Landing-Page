c3ed6819a18353861e57b2a41b17f5f639ffe787
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-08-09 11:28:15 -0300

    seo

--- javascript/module/scrollHeader.js
+++ javascript/module/scrollHeader.js
@@ -1,14 +1,17 @@
-export default function scrollHeader() {}
-
-const mulherLoira = document.querySelector('[data-img="mulherLoira"]');
+export default function scrollHeader() {
+  const mulherLoira = document.querySelector('[data-img="mulherLoira"]');
 const containerIntro = document.querySelector('[data-container="intro"]');
 const textIntro = document.querySelectorAll('[data-text="intro"]');
+const drInfo = document.querySelectorAll('[data-infor="dr"]');
+const containerDrInfo = document.querySelector('[data-container="dadosProfissional"]');
+const containerFormacao = document.querySelector('[data-container="formacao"]');
+const cardFormacao = document.querySelectorAll('[data-formacao="item"]');
+
 let alturaDisplay = window.innerHeight * 0.7;
 
 function mostrar() {
   mulherLoira.classList.add("mostrar-img");
 }
-
 
 function mostrarScroll() {
   if (containerIntro.getBoundingClientRect().top <= 0) {
@@ -19,28 +22,18 @@ function mostrarScroll() {
     });
   }
 }
-//
 
-const drInfo = document.querySelectorAll('[data-infor="dr"]')
-const containerDrInfo = document.querySelector('[data-container="dadosProfissional"]')
-
-function mostrarDadosDr(){
-    if (containerDrInfo.getBoundingClientRect().top <= alturaDisplay) {
-        drInfo.forEach((item, index) => {
-          setTimeout(() => {
-            item.classList.add("mostrar-formacao");
-          }, 500 * index);
-        });
-      }
+function mostrarDadosDr() {
+  if (containerDrInfo.getBoundingClientRect().top <= alturaDisplay) {
+    drInfo.forEach((item, index) => {
+      setTimeout(() => {
+        item.classList.add("mostrar-formacao");
+      }, 500 * index);
+    });
+  }
 }
 
-//
-
-const containerFormacao = document.querySelector('[data-container="formacao"]');
-const cardFormacao = document.querySelectorAll('[data-formacao="item"]');
-
-
-function mostrarScrollFormação() {
+function mostrarScrollFormacao() {
   if (containerFormacao.getBoundingClientRect().top <= alturaDisplay) {
     cardFormacao.forEach((item, index) => {
       setTimeout(() => {
@@ -49,29 +42,28 @@ function mostrarScrollFormação() {
     });
   }
 }
-//
-//função para versao PC
-function mostrarTextIntro(){
-  mulherLoira.classList.add("mostrar-img");
+
+function mostrarTextIntro() {
+  mostrar();
   textIntro.forEach((item, index) => {
     setTimeout(() => {
       item.classList.add("mostrar-texto");
     }, 500 * index);
   });
-  
 }
-
 
 function telaCelular() {
   if (window.outerWidth <= 800) {
-    mostrar()
+    mostrar();
     window.addEventListener("scroll", mostrarScroll);
-    window.addEventListener("scroll", mostrarScrollFormação)
+    window.addEventListener("scroll", mostrarScrollFormacao);
     window.addEventListener("scroll", mostrarDadosDr);
-  }else{
-    mostrarTextIntro()
+  } else {
+    mostrarTextIntro();
   }
 }
 
 window.addEventListener("load", telaCelular);
+
+}
 
--- javascript/module/slide.js
+++ javascript/module/slide.js
@@ -1,91 +1,80 @@
-export default function slide(){
-    const slideGeral = document.querySelector('[data-slide="geral"]')
-const slideLista = document.querySelector('[data-slide="lista"]')
-const slideItem = document.querySelectorAll('[data-slide="item"]')
-const slideVoltar = document.querySelector('[data-slide="btn-voltar"]')
-const slideProximo = document.querySelector('[data-slide="btn-proximo"]')
-let slideAuto
-
-let contador = 0
-let maximoClick = 0
-
-
-function proximo(event){
-    limparIniciarInterval()
-    const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18
-    slideLista.scrollLeft += distanciaMovimentada;
-    console.log(distanciaMovimentada)
-    contador++
-    if(contador == maximoClick){
-        slideLista.scrollLeft = 0
-        contador = 0
+export default function slide() {
+    const slideGeral = document.querySelector('[data-slide="geral"]');
+    const slideLista = document.querySelector('[data-slide="lista"]');
+    const slideItem = document.querySelectorAll('[data-slide="item"]');
+    const slideVoltar = document.querySelector('[data-slide="btn-voltar"]');
+    const slideProximo = document.querySelector('[data-slide="btn-proximo"]');
+  
+    let slideAuto;
+    let contador = 0;
+    let maximoClick = 0;
+  
+    function proximo(event) {
+      limparIniciarInterval();
+      const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18;
+      slideLista.scrollLeft += distanciaMovimentada;
+      contador++;
+      if (contador === maximoClick) {
+        slideLista.scrollLeft = 0;
+        contador = 0;
+      }
     }
-}
-
-function voltar(evento){
-    limparIniciarInterval()
-    const distanciaVoltar = slideItem[0].getBoundingClientRect().width + 20
-    slideLista.scrollLeft -= distanciaVoltar;
-    console.log(distanciaMovimentada)
-    contador--
-}
-
-function telaCelular(){
-    console.log("funcionou")
-    if(window.outerWidth <= 800){
-       slideAuto = setInterval(() =>{
-            const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18
-            slideLista.scrollLeft += distanciaMovimentada;
-            contador++
-            if(contador == maximoClick){
-                slideLista.scrollLeft = 0
-                contador = 0
-            }
-        }, 2500)
-    }else{
-        slideAuto = setInterval(() =>{
-            const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18
-            slideLista.scrollLeft += distanciaMovimentada;
-            contador++
-            if(contador == maximoClick){
-                slideLista.scrollLeft = 0
-                contador = 0
-            }
-        }, 2000)
+  
+    function voltar(evento) {
+      limparIniciarInterval();
+      const distanciaVoltar = slideItem[0].getBoundingClientRect().width + 20;
+      slideLista.scrollLeft -= distanciaVoltar;
+      contador--;
     }
-   
-}
-function limparIniciarInterval(){
-    clearInterval(slideAuto)
-    slideAuto = setInterval(() =>{
-        const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18
+  
+    function telaCelular() {
+      if (window.outerWidth <= 800) {
+        slideAuto = setInterval(() => {
+          const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18;
+          slideLista.scrollLeft += distanciaMovimentada;
+          contador++;
+          if (contador === maximoClick) {
+            slideLista.scrollLeft = 0;
+            contador = 0;
+          }
+        }, 2500);
+      } else {
+        slideAuto = setInterval(() => {
+          const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18;
+          slideLista.scrollLeft += distanciaMovimentada;
+          contador++;
+          if (contador === maximoClick) {
+            slideLista.scrollLeft = 0;
+            contador = 0;
+          }
+        }, 2000);
+      }
+    }
+  
+    function limparIniciarInterval() {
+      clearInterval(slideAuto);
+      slideAuto = setInterval(() => {
+        const distanciaMovimentada = slideItem[0].getBoundingClientRect().width + 18;
         slideLista.scrollLeft += distanciaMovimentada;
-        contador++
-        if(contador == maximoClick){
-            slideLista.scrollLeft = 0
-            contador = 0
+        contador++;
+        if (contador === maximoClick) {
+          slideLista.scrollLeft = 0;
+          contador = 0;
         }
-    }, 2500)
-}
-
-
-function numeroClick(){
-    if(window.outerWidth <= 800){
-        maximoClick = 5
-    }else{
-        maximoClick = 3
+      }, 2500);
     }
-}
-
-slideProximo.addEventListener("click", proximo)
-slideVoltar.addEventListener("click", voltar)
-window.addEventListener("load", numeroClick)
-window.addEventListener("load", telaCelular)
-
-
-
-}
-
-
-
-
+  
+    function numeroClick() {
+      if (window.outerWidth <= 800) {
+        maximoClick = 5;
+      } else {
+        maximoClick = 3;
+      }
+    }
+  
+    slideProximo.addEventListener("click", proximo);
+    slideVoltar.addEventListener("click", voltar);
+    window.addEventListener("load", numeroClick);
+    window.addEventListener("load", telaCelular);
+  }
+  
